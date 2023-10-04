764e565fd322fb00449ffc01a5511b4ae0b084db
Tsuyoshi Toyofuku <2111140087w@ed.fuk.kindai.ac.jp>
2023-10-04 06:55:25 +0000

    create clone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8b3b69d7e9314aae89d0e5c49c93519b.vfs.cloud9.us-east-1.amazonaws.com"
 end
